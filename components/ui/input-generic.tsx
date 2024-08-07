13d4faa59e68641bbd67985e64f103acedf1397a
Hyago Bitencourt <164915188+VouganHyago@users.noreply.github.com>
2024-08-07 06:50:03 -0300

    fix: input ring

--- components/ui/input-generic.tsx
+++ components/ui/input-generic.tsx
@@ -15,7 +15,7 @@ export const InputGeneric = ({
     defaultValue={defaultValue}
     {...props}
     className={cn(
-      "w-full rounded-sm border-[1px] border-zinc-400/50 bg-basis p-2 focus:outline-none focus:ring-1 focus:ring-primary",
+      "w-full rounded-sm border-[1px] border-zinc-400/50 bg-basis p-2 focus:outline-none focus:ring-2 focus:ring-primary",
       className,
     )}
   />
